210916fc20dd5970c38df1fbf9371055b1735d8b
Saquib Hasan <saquibhasan3@gmail.com>
2023-09-27 17:57:55 +0530

    Package card changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,33 +9,38 @@
                         <div class="card-header">{{ __('Travel Packages') }}</div>
                         <div class="card-body">
                             <div class="container m-4">
-                                @foreach ($travel_packages as $package)
-                                    <div class="card border-0 rounded-0 shadow" style="width: 18rem;">
-                                        <a href="{{ route('travel_package.details', ['package_slug' => $package->package_slug]) }}">
-                                            <img src="{{ asset('uploads/'.$package->package_image) }}" onerror="this.src='{{ asset('assets/images/no-image.jpg') }}'" class="card-img-top rounded-0" alt="Travel Package Image">
-                                        </a>
-                                        <div class="card-body mt-3 mb-3">
-                                            <div class="row">
-                                                <div class="col-12">
+                                <div class="row">
+                                    @foreach ($travel_packages as $package)
+                                    <div class="col-md-4">
 
-                                                    <h4 class="card-title">
-                                                        <a href="{{ route('travel_package.details', ['package_slug' => $package->package_slug]) }}" class="nostyle">
-                                                            {{ $package->package_name }}    
-                                                        </a>
-                                                    </h4>
+                                        <div class="card border-0 rounded-0 shadow" style="width: 18rem;">
+                                            <a href="{{ route('travel_package.details', ['package_slug' => $package->package_slug]) }}">
+                                                <img src="{{ asset('uploads/'.$package->package_image) }}" onerror="this.src='{{ asset('assets/images/no-image.jpg') }}'" class="card-img-top rounded-0" alt="Travel Package Image">
+                                            </a>
+                                            <div class="card-body mt-3 mb-3">
+                                                <div class="row">
+                                                    <div class="col-12">
+
+                                                        <h4 class="card-title">
+                                                            <a href="{{ route('travel_package.details', ['package_slug' => $package->package_slug]) }}" class="nostyle">
+                                                                {{ $package->package_name }}    
+                                                            </a>
+                                                        </h4>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row align-items-center text-center g-0">
+                                                <div class="col-6">
+                                                    <h5><i class="fa fa-inr"></i> {{ $package->price }}</h5>
+                                                </div>
+                                                <div class="col-6">
+                                                    <a href="{{ route('travel_package.details', ['package_slug' => $package->package_slug]) }}" class="btn btn-dark w-100 p-3 rounded-0 text-warning">{{ __('View Details') }}</a>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="row align-items-center text-center g-0">
-                                            <div class="col-6">
-                                                <h5><i class="fa fa-inr"></i> {{ $package->price }}</h5>
-                                            </div>
-                                            <div class="col-6">
-                                                <a href="{{ route('travel_package.details', ['package_slug' => $package->package_slug]) }}" class="btn btn-dark w-100 p-3 rounded-0 text-warning">{{ __('View Details') }}</a>
-                                            </div>
-                                        </div>
                                     </div>
-                                @endforeach
+                                    @endforeach
+                                </div>
                             </div>
                         </div>
                     @else
